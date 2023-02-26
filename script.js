80722a06b2b465179d30f406255db6108fce601d
Roderen <mikilikala@gmail.com>
2023-02-26 21:20:39 +0200

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+const a = parseInt(prompt('First num'));
+const b = parseInt(prompt('Second num'));
+
+function operators(a, b) {
+  console.log(a + b);
+  console.log(a - b);
+  console.log(a * b);
+  console.log(a / b);
+}
+operators(a, b);
