e28181d7cbc9e628192a48502fcd077dcdb3013b
Roderen <mikilikala@gmail.com>
2023-02-28 17:14:42 +0200

    added functions

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const a = parseInt(prompt('First num'));
 const b = parseInt(prompt('Second num'));
+sum(a, b);
+minus(a, b);
+multiply(a, b);
+divide(a, b);
 
-function operators(a, b) {
+function sum(a, b) {
   console.log(a + b);
+}
+
+function minus(a, b) {
   console.log(a - b);
+}
+
+function multiply(a, b) {
   console.log(a * b);
+}
+
+function divide(a, b) {
   console.log(a / b);
 }
-operators(a, b);
